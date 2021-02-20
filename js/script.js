934e7f775c645ad0d7021450389f0d061333d15b
weescoelho <wes.coelho18@gmail.com>
2021-02-20 12:43:47 -0300

    alterado ordem scroll animacao

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import SoftScroll from './modules/SoftScroll.js';
-import initAnimationScroll from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
@@ -8,6 +7,7 @@ import initDropdownMenu from './modules/dropdown.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initNumberAnim from './modules/numbersAnim.js';
 import initFuncionamento from './modules/funcionamento.js';
+import initAnimationScroll from './modules/scroll-animation.js';
 
 const softScroll = new SoftScroll('[data-soft="smooth"] a[href^="#"]');
 softScroll.init();
