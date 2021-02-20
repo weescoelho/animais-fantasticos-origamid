daebb45429d1364a1b08611ade5f097e9cd00c2a
weescoelho <wes.coelho18@gmail.com>
2021-02-20 12:37:48 -0300

    scrollsuave refatorado - criação da classe

---
+++ js/modules/SoftScroll.js
@@ -0,0 +1,31 @@
+export default class SoftScroll {
+  constructor(links, options) {
+    this.internalLink = document.querySelectorAll(links);
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
+
+  addLinkEvent() {
+    this.internalLink.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.internalLink.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
+}
